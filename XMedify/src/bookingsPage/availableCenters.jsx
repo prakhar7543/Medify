cb11d89d6ff60800db7879b382dd3bd1cecf929c
Your Name <you@example.com>
2025-06-24 17:26:36 +0530

    add

--- XMedify/src/bookingsPage/availableCenters.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import './availableCenters.css';
-
-export default function AvailableCenters(){
-    
-    return(
-        <div></div>
-    )
-}
--- XMedify/src/bookingsPage/myBookings.jsx
+++ XMedify/src/bookingsPage/myBookings.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import TopBanner from '../components/heroSectionComponents/topBanner'
 // import BookingsNavbar from '../bookingsPage/tabsNavbar'
-import MyBookingsNavbar from '../bookingsPage/MyBookingsNavbar';
+import MyBookingsNavbar from './myBookingsNavbar';
 import MyBookingsHeroSection from '../bookingsPage/myBookingsHeroSection'
 
 export default function MyBookings(){
